4c786f7671806a00bb88c25e1726cb5e0259fc00
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2022-12-16 20:45:32 +0100

    ink! 4

--- src/ink/mod.rs
+++ src/ink/mod.rs
@@ -20,11 +20,10 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #[cfg(feature = "std")]
-pub use ink_engine;
-pub use ink_env;
-pub use ink_lang;
-#[cfg(feature = "std")]
-pub use ink_metadata;
-pub use ink_prelude;
-pub use ink_primitives;
-pub use ink_storage;
+pub use ink_lang::metadata;
+pub use ink_lang::{
+    env,
+    prelude,
+    primitives,
+    storage,
+};
